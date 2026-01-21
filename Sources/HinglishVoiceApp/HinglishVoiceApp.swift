ddcc8956e3a296e0e33e6f3bbf3fdbe621670708
Aaravanand <aaravanand@gmail.com>
2026-01-21 17:50:00 +0530

    Add main app entry point HinglishVoiceApp.swift

---
+++ Sources/HinglishVoiceApp/HinglishVoiceApp.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+@main
+public struct HinglishVoiceApp: App {
+    public init() {}
+    
+    public var body: some Scene {
+        WindowGroup {
+            MainAssistantView()
+        }
+    }
+}
